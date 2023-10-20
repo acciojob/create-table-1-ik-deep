48c1c98bb2079f1cce663ce0ea2a21fbccb10382
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-20 21:39:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ function insert_Row() {
     //Write your code here
 	const table = document.getElementById("sampleTable");
   let newtr = document.createElement("tr");
-const row = `<td>New Cell1</td> 
-  <td>New Cell2</td>`;
-newtr.innerHTML=row;
+	let td1 = document.createElement("td");
+	td1.innerText="New Cell1"
+   let td2 = document.createElement("td");
+	td2.innerText = "New Cell2"
+// const row = `<td>New Cell1</td> 
+//   <td>New Cell2</td>`;
+// newtr.innerHTML=row;
+	newtr.appendChild(td1);
+	newtr.appendChild(td2);
 table.prepend(newtr) ;
   
   
